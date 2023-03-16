79f7d22752c53d409f74e9969cbf4ca732a0057f
Malbathan <malbathanlima@gmail.com>
2023-03-16 12:41:35 -0300

    feat: :sparkles: Maps
    Map created and renderized on the site

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Map from './pages/Map';
 
 function App() {
   return (
     <div className="App">
-
+      <Map />
     </div>
   );
 }
