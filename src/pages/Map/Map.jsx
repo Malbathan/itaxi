bbae4b4a9e1a569a4d2c9f93d5f944cd15bea3d4
Malbathan <malbathanlima@gmail.com>
2023-03-17 14:17:24 -0300

    feat: :sparkles: Deploy commit

--- src/pages/Map/Map.jsx
+++ src/pages/Map/Map.jsx
@@ -1,51 +1,42 @@
-import { useRef, useState, useEffect} from 'react'
+import { useRef, useState, useEffect } from 'react'
 import { Style } from './MapStyle'
-import  car1 from '../../assets/car1.png'
-import  car2 from '../../assets/car2.png'
-import  car3 from '../../assets/car3.png'
+import './Map.css'
 import {
-  Box, Button, ButtonGroup, Flex, Modal, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, 
+  Box, Button, ButtonGroup, Flex, Modal, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter,
   ModalOverlay, HStack, VStack, IconButton, Input, Image, useDisclosure, Text, RadioGroup, Radio
 } from '@chakra-ui/react'
-
 import { FaLocationArrow, FaTimes } from 'react-icons/fa'
 
-import {
-  useJsApiLoader,
-  GoogleMap,
-  Marker,
-  Autocomplete,
-  DirectionsRenderer,
-} from '@react-google-maps/api'
+import car1 from '../../assets/car1.png'
+import car2 from '../../assets/car2.png'
+import car3 from '../../assets/car3.png'
+
+import * as api from '@react-google-maps/api'
+
+const libraries = ['places']
 
 function Map() {
-  const { isLoaded } = useJsApiLoader({
+  const { isLoaded } = api.useJsApiLoader({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-    libraries: ['places'],
+    libraries: libraries,
   })
-
-  const OverlayOne = () => (
-    <ModalOverlay
-      bg='blackAlpha.300'
-      backdropFilter='blur(10px) hue-rotate(90deg)'
-    />
-  )
 
   const [map, setMap] = useState(/** @type google.maps.Map */(null))
   const [directionsResponse, setDirectionsResponse] = useState(null)
+  const [isOpenDriver, setIsOpenDriver] = useState(false)
+  const [disable, setDisable] = useState(true)
   const [distance, setDistance] = useState('')
   const [duration, setDuration] = useState('')
   const [value, setValue] = useState('')
-  const [price, setPrice] = useState({ priceX : '', priceComfort : '', priceDeluxe : ''})
+  const [price, setPrice] = useState({ priceX: '', priceComfort: '', priceDeluxe: '' })
   const [userPos, setUserPos] = useState({ lat: -15.790021, lng: -47.883702 })
-  const {isOpen, onOpen, onClose } = useDisclosure()
-  const [overlay, setOverlay] = useState(<OverlayOne />)
-
+  const {isOpen, onOpen, onClose} = useDisclosure()
   const originRef = useRef()
   const destinationRef = useRef()
-  
 
+  // Posição inicial do Mapa.
   useEffect(() => {
+
     navigator.geolocation.getCurrentPosition((pos) => {
       const newUserPos = {
         lat: pos.coords.latitude,
@@ -55,45 +46,56 @@ function Map() {
     }, (err) => {
       console.log(err);
     });
-    
 
   }, [])
 
-
+  // Efeito ao traçar rota e trazer valores 
   useEffect(() => {
+
     const TableOfPrices = {
       x: 1.20,
-      comfort:  2.20,
+      comfort: 2.20,
       deluxe: 3.20
     }
-    if(!!distance){
-      let distanceNumber = parseFloat(distance.replace(',','.'));
+    if (!!distance) {
+      let distanceNumber = parseFloat(distance.replace(',', '.'));
       let durationNumber = parseFloat(duration.replace(/\D/gim, ''));
-      
 
-      let price1 = distanceNumber + durationNumber /2 * TableOfPrices.x      
-      let price2 = distanceNumber + durationNumber /2 * TableOfPrices.comfort
-      let price3 = distanceNumber + durationNumber /2 * TableOfPrices.deluxe
+
+      let price1 = distanceNumber + durationNumber / 2 * TableOfPrices.x
+      let price2 = distanceNumber + durationNumber / 2 * TableOfPrices.comfort
+      let price3 = distanceNumber + durationNumber / 2 * TableOfPrices.deluxe
 
       const newPrice = {
-        priceX : price1.toFixed(2),
-        priceComfort : price2.toFixed(2),
-        priceDeluxe : price3.toFixed(2)
+        priceX: price1.toFixed(2),
+        priceComfort: price2.toFixed(2),
+        priceDeluxe: price3.toFixed(2)
       }
-      
+
       setPrice(newPrice)
     }
 
-  }, [distance,duration])
+  }, [distance, duration])
+
+  // Ativar botão de escolha de carro
+  useEffect(() => {
+
+    if (value.length > 0) {
+      setDisable(false)
+    }
+
+  }, [value])
 
   if (!isLoaded) {
     return null
   }
 
   async function calculateRoute() {
+
     if (originRef.current.value === '' || destinationRef.current.value === '') {
       return
     }
+
     // eslint-disable-next-line no-undef
     const directionsService = new google.maps.DirectionsService()
     const results = await directionsService.route({
@@ -102,29 +104,41 @@ function Map() {
       // eslint-disable-next-line no-undef
       travelMode: google.maps.TravelMode.DRIVING,
     })
+
     setDirectionsResponse(results)
     setDistance(results.routes[0].legs[0].distance.text)
     setDuration(results.routes[0].legs[0].duration.text)
   }
 
   function clearRoute() {
+
     setDirectionsResponse(null)
     setDistance('')
     setDuration('')
     originRef.current.value = ''
     destinationRef.current.value = ''
-    setPrice({priceX : '', priceComfort : '', priceDeluxe : ''});
+    setPrice({ priceX: '', priceComfort: '', priceDeluxe: '' });
+
   }
 
 
-  function handleClick(){
-    if(originRef.current.value && destinationRef.current.value){
-      calculateRoute(); 
-      setOverlay(<OverlayOne />)
+  function routeHandleClick() {
+
+    if (originRef.current.value && destinationRef.current.value) {
+      calculateRoute();
       onOpen()
-    } 
+    }
+
   }
 
+  function handleGoodChoise() {
+    onClose()
+    setIsOpenDriver(true)
+  }
+
+  function closeDriverModal() {
+    setIsOpenDriver(false)
+  }
 
   return (
     <Flex
@@ -135,8 +149,8 @@ function Map() {
       w='100vw'
     >
       <Box position='absolute' left={0} top={0} h='100%' w='100%'>
-        {/* Google Map Box */}
-        <GoogleMap
+
+        <api.GoogleMap
           center={userPos}
           zoom={18}
           mapContainerStyle={{ width: '100%', height: '100%' }}
@@ -149,11 +163,11 @@ function Map() {
           }}
           onLoad={map => setMap(map)}
         >
-          <Marker position={userPos} />
+          <api.Marker position={userPos} />
           {directionsResponse && (
-            <DirectionsRenderer directions={directionsResponse} />
+            <api.DirectionsRenderer directions={directionsResponse} />
           )}
-        </GoogleMap>
+        </api.GoogleMap>
       </Box>
       <Box
         p={4}
@@ -166,26 +180,26 @@ function Map() {
       >
         <HStack spacing={2} justifyContent='space-between'>
           <Box flexGrow={1}>
-            <Autocomplete>
+            <api.Autocomplete>
               <Input type='text' placeholder='Origem' ref={originRef} />
-            </Autocomplete>
+            </api.Autocomplete>
           </Box>
           <Box flexGrow={1}>
-            <Autocomplete>
+            <api.Autocomplete>
               <Input
                 type='text'
                 placeholder='Destino'
                 ref={destinationRef}
               />
-            </Autocomplete>
+            </api.Autocomplete>
           </Box>
 
           <ButtonGroup>
-            <Button 
-              colorScheme='pink' 
-              type='submit' 
-              onClick={handleClick} 
-              >
+            <Button
+              colorScheme='pink'
+              type='submit'
+              onClick={routeHandleClick}
+            >
               Rotas
             </Button>
             <IconButton
@@ -196,8 +210,8 @@ function Map() {
           </ButtonGroup>
         </HStack>
         <HStack spacing={4} mt={4} justifyContent='space-between'>
-          <Text>Distancia: {distance} </Text>
-          <Text>Duração: {duration} </Text>
+          <Text as='b'>Distancia: {distance} </Text>
+          <Text as='b'>Duração: {duration} </Text>
           <IconButton
             aria-label='center back'
             icon={<FaLocationArrow />}
@@ -208,45 +222,67 @@ function Map() {
             }}
           />
         </HStack>
+
         <Modal isCentered isOpen={isOpen} onClose={onClose}>
-        {overlay}
-        <ModalContent>
-          <ModalHeader>Escolha sua caranga! ;) </ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-          <RadioGroup onChange={setValue} value={value}>
-              <VStack >
-                <Radio value='1'>
-                  <HStack>
-                    <Image src={car1} boxSize='150px'  objectFit='full' alt='fusca azul' />
-                    <Text> X-fusca </Text>
-                    <Text>R${price.priceX}</Text>
-                  </HStack>
-                </Radio>
-                <Radio value='2'>
-                  <HStack>
-                    <Image src={car2} boxSize='150px'  objectFit='full' alt='Combi laranja' />
-                    <Text> Combi-Comfort </Text>
-                    <Text>R${price.priceComfort}</Text>
-                  </HStack>
-                </Radio>
-                <Radio value='3'>
-                    <HStack>
-                      <Image src={car3} boxSize='150px'  objectFit='full' alt='carro de luxo vintage' />
-                      <Text> Deluxe </Text>
-                      <Text>R${price.priceDeluxe }</Text>
+          <ModalOverlay
+            bg='blackAlpha.300'
+            backdropFilter='blur(10px) hue-rotate(90deg)'
+          />
+          <ModalContent>
+            <ModalHeader>Escolha sua caranga! ;) </ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <RadioGroup onChange={setValue} value={value} width="100%">
+                <VStack  width="100%" >
+                  <Radio value='1' width="100%" >
+                    <HStack  width="100%">
+                      <Image src={car1} boxSize='100px' objectFit='full' alt='fusca azul' />
+                      <Text as='b'> X-fusca </Text>
+                      <Text as='b' ml='auto'>R${price.priceX}</Text>
                     </HStack>
-                </Radio>
-              </VStack>
-            </RadioGroup> 
-          </ModalBody>
-          <ModalFooter>
-            <Button onClick={onClose} disabled={true}>Boa escolha!</Button>
-            <Button onClick={onClose}>Fechar</Button>
-          </ModalFooter>
-        </ModalContent>
+                  </Radio>
+                  <Radio value='2' width="100%" >
+                    <HStack width="100%">
+                      <Image src={car2} boxSize='100px' objectFit='full' alt='Combi laranja' />
+                      <Text as='b'> Combi-Comfort </Text>
+                      <Text as='b' ml='auto'>R${price.priceComfort}</Text>
+                    </HStack>
+                  </Radio>
+                  <Radio value='3' width="100%" >
+                    <HStack  width="100%">
+                      <Image src={car3} boxSize='100px' objectFit='full' alt='carro de luxo vintage' />
+                      <Text as='b'> Cadilac - Bip Bip! </Text>
+                      <Text as='b' ml='auto'>R${price.priceDeluxe}</Text>
+                    </HStack>
+                  </Radio>
+                </VStack>
+              </RadioGroup>
+            </ModalBody>
+            <ModalFooter>
+              <Button onClick={handleGoodChoise} disabled={disable} mr="2" colorScheme='yellow' >Boa escolha!</Button>
+              <Button onClick={onClose}>Fechar</Button>
+            </ModalFooter>
+          </ModalContent>
         </Modal>
       </Box>
+
+      <Modal isCentered isOpen={isOpenDriver} onClose={closeDriverModal} >
+        <ModalOverlay
+          bg='none'
+          backdropFilter='auto'
+          backdropInvert='80%'
+          backdropBlur='2px'
+        />
+        <ModalContent>
+          <ModalHeader>Seu motora está a caminho! ;)</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Image src='https://pbs.twimg.com/media/D_jKTlLXoAAYUS-.jpg:large' alt='Agostinho Carrara'>
+
+            </Image>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     </Flex>
   )
 }
